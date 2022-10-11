c4de2d401d69c887f784c2cc532b31375600d9b8
Nikhil-Koushik <115442904+Nikhil-Koushik@users.noreply.github.com>
2022-10-11 09:38:04 +0530

    Create Random.py

---
+++ Random.py
@@ -0,0 +1,10 @@
+import random
+n=int(input("Enter no.of players:"))
+a=[]
+b=["Truth","Dare"]
+for i in range(0,n):
+    a.insert(i,input("Enter name %d:"%(i+1)))
+x=random.randrange(0,n)
+y=random.randrange(0,2)
+print(a[x])
+print(b[y])
