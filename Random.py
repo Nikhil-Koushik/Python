e39bedcf4ecab84393cd8bb6d19fd850c9aadf87
Parimala26 <115444046+Parimala26@users.noreply.github.com>
2022-10-11 12:07:23 +0530

    Update Random.py

--- Random.py
+++ Random.py
@@ -4,7 +4,7 @@ a=[]
 b=["Truth","Dare"]
 for i in range(0,n):
     a.insert(i,input("Enter name %d:"%(i+1)))
-x=random.randrange(0,n)
-y=random.randrange(0,2)
-print(a[x])
-print(b[y])
+k=random.randrange(0,n)
+r=random.randrange(0,2)
+print(a[k])
+print(b[r])
